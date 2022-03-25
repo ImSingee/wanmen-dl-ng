b5c01c1e0c14651e144861a0c9a807706c01a095
Singee <git@singee.me>
2022-03-25 22:57:38 +0800

    get-all support shuffle
    Signed-off-by: Singee <git@singee.me>

--- list.go
+++ list.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 func getList(filename string) ([]string, error) {
+	random := false
+	if strings.HasSuffix(filename, "?") {
+		random = true
+		filename = strings.TrimSuffix(filename, "?")
+	}
+
 	f, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, fmt.Errorf("cannot read file: %w", err)
@@ -25,7 +33,17 @@ func getList(filename string) ([]string, error) {
 		list = append(list, line)
 	}
 
+	if random {
+		rand.Shuffle(len(list), func(i, j int) {
+			list[i], list[j] = list[j], list[i]
+		})
+	}
+
 	return list, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 var list []string
